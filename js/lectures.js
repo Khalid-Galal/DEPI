02c4ec0f56758c605d6b974389c79737f5a39477
khaled Galal <111221802+Khalid-Galal@users.noreply.github.com>
2024-11-25 16:18:12 +0200

    update lectures

--- js/lectures.js
+++ js/lectures.js
@@ -126,24 +126,24 @@ const lectureData = [
                 label: "Lecture 9 (Video)", 
                 category: "Technical" 
             },
-            // { 
-            //     type: "PDF", 
-            //     url: "https://drive.google.com/file/d/1h0x-VD9zwYS8uCR9YgenufLRBdyudEOU/view?usp=drivesdk", 
-            //     label: "Lecture 9 (PDF)", 
-            //     category: "Technical" 
-            // },
+            { 
+                type: "PDF", 
+                url: "https://drive.google.com/file/d/1ZMGLIXNu0rpao7R3ayi7Igl5hcKBDcXT/view?usp=drive_link", 
+                label: "Lecture 9 (PDF)", 
+                category: "Technical" 
+            },
             { 
                 type: "Video", 
                // url: "https://itccollegprot039-my.sharepoint.com/:v:/g/personal/ghr66_itccollegprot039_onmicrosoft_com/EXD18Fd76dxGpfCVoxSNYOwB-C348MVN1sEuUUb1d8Dl1A?e=JI8B6L&nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJTdHJlYW1XZWJBcHAiLCJyZWZlcnJhbFZpZXciOiJTaGFyZURpYWxvZy1MaW5rIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXcifX0%3D", 
                 label: "Lecture 10 offline", 
                 category: "Technical" 
             },
-            // { 
-            //     type: "PDF", 
-            //     url: "https://drive.google.com/file/d/1hvOdp9Mmb0R38SGJdtTJQzQ4Gm-UTwg9/view?usp=drivesdk", 
-            //     label: "Lecture 10 (PDF)", 
-            //     category: "Technical" 
-            // },
+            { 
+                type: "PDF", 
+                url: "https://drive.google.com/file/d/18jzHB65FlcgvPslU-ybDbKOa7plhLNZo/view?usp=drive_link", 
+                label: "Lecture 10 (PDF)", 
+                category: "Technical" 
+            },
             { 
                 type: "Video", 
                 url: "https://vimeo.com/1029716657/e6a1b2c22d?share=copy", 
@@ -173,24 +173,24 @@ const lectureData = [
                 label: "Lecture 12 (Video)", 
                 category: "Technical" 
             },
-            // { 
-            //     type: "PDF", 
-            //     url: "https://drive.google.com/file/d/1h0x-VD9zwYS8uCR9YgenufLRBdyudEOU/view?usp=drivesdk", 
-            //     label: "Lecture 6 (PDF)", 
-            //     category: "Technical" 
-            // },
+            { 
+                type: "PDF", 
+                url: "https://drive.google.com/file/d/1OYOHLujNfrjm8n6Q6V82KJrZ9NZftEro/view?usp=drive_link", 
+                label: "Lecture 12 (PDF)", 
+                category: "Technical" 
+            },
             { 
                 type: "Video", 
                 url: "https://itccollegprot039-my.sharepoint.com/personal/ghr66_itccollegprot039_onmicrosoft_com/_layouts/15/stream.aspx?id=%2Fpersonal%2Fghr66%5Fitccollegprot039%5Fonmicrosoft%5Fcom%2FDocuments%2FRecordings%2FVulnerability%20Analyst%20%20Penetration%20Tester%20%2D%20GLB%20%2D%20CAI2%5FISS3%5FG1%20%2D%203H%2D20241122%5F093710%2DMeeting%20Recording%2Emp4&nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJTdHJlYW1XZWJBcHAiLCJyZWZlcnJhbFZpZXciOiJTaGFyZURpYWxvZy1MaW5rIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXcifX0&ga=1&referrer=StreamWebApp%2EWeb&referrerScenario=AddressBarCopied%2Eview%2Edbaae783%2D0255%2D40f3%2Da2bf%2D36ae1dc9371d", 
                 label: "Lecture 13 (Video)", 
                 category: "Technical" 
             }
-            // { 
-            //     type: "PDF", 
-            //     url: "https://drive.google.com/file/d/1hvOdp9Mmb0R38SGJdtTJQzQ4Gm-UTwg9/view?usp=drivesdk", 
-            //     label: "Lecture 7 (PDF)", 
-            //     category: "Technical" 
-            // },
+            { 
+                type: "PDF", 
+                url: "https://drive.google.com/file/d/1bRjdnFj29ptUuUKKFKaskroHT1oG77gO/view?usp=drive_link", 
+                label: "Lecture 13 (PDF)", 
+                category: "Technical" 
+            },
             // { 
             //     type: "Video", 
             //     url: "https://itccollegprot039-my.sharepoint.com/personal/ghr66_itccollegprot039_onmicrosoft_com/_layouts/15/stream.aspx?id=%2Fpersonal%2Fghr66%5Fitccollegprot039%5Fonmicrosoft%5Fcom%2FDocuments%2FRecordings%2FVulnerability%20Analyst%20%20Penetration%20Tester%20%2D%20GLB%20%2D%20CAI2%5FISS3%5FG1%20%2D%203H%2D20241109%5F193638%2DMeeting%20Recording%2Emp4&nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJTdHJlYW1XZWJBcHAiLCJyZWZlcnJhbFZpZXciOiJTaGFyZURpYWxvZy1MaW5rIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXcifX0&ga=1&referrer=StreamWebApp%2EWeb&referrerScenario=AddressBarCopied%2Eview%2E80ca8af6%2Db99c%2D4110%2Dad39%2D1b11e30322d0", 
