f5bac0863c5ab6d6dca8aed82146fa77a697dd92
khaled Galal <111221802+Khalid-Galal@users.noreply.github.com>
2024-12-23 13:52:56 +0200

    Add more lec

--- js/lectures.js
+++ js/lectures.js
@@ -190,19 +190,191 @@ const lectureData = [
                 url: "https://drive.google.com/file/d/1bRjdnFj29ptUuUKKFKaskroHT1oG77gO/view?usp=drive_link", 
                 label: "Lecture 13 (PDF)", 
                 category: "Technical" 
+            },
+            { 
+                type: "Video", 
+                url: "https://itccollegprot039-my.sharepoint.com/:v:/g/personal/ghr66_itccollegprot039_onmicrosoft_com/EUkzbrL9XntMj5LVGzSzKHoBMpHNp0HiWEADmN0T07dEgg?e=xfVxzm&nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJTdHJlYW1XZWJBcHAiLCJyZWZlcnJhbFZpZXciOiJTaGFyZURpYWxvZy1MaW5rIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXcifX0%3D", 
+                label: "Lecture 14 (Video)", 
+                category: "Non-Technical" 
+            },
+            { 
+                type: "PDF", 
+                url: "https://docs.google.com/presentation/d/16sCl7FJOPYtDdqHp14OlDAIqdBr-s9tt/edit", 
+                label: "presentation Lecture 14 (PDF)", 
+                category: "Non-Technical" 
             }
+        ]
+    },
+    {
+        week: "Week 6",
+        lectures: [
+            { 
+                type: "Video", 
+                url: "https://itccollegprot039-my.sharepoint.com/:v:/g/personal/ghr66_itccollegprot039_onmicrosoft_com/EWjxBX_P3FpHjshBa0ykygEBPj51-J9kmzFSkMAoKF3_ag?e=xhYupN&nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJTdHJlYW1XZWJBcHAiLCJyZWZlcnJhbFZpZXciOiJTaGFyZURpYWxvZy1MaW5rIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXcifX0%3D", 
+                label: "Reporting and Documentation Lecture 15 (Video)", 
+                category: "Technical" 
+            },
+            { 
+                type: "PDF", 
+                url: "https://drive.google.com/drive/folders/1yOVojPL9px3LDCWC6sAjmZqhUKAT1nDn", 
+                label: "Reporting and Documentation Lecture 15 (PDF)", 
+                category: "Technical" 
+                //28/11/2024
+            },
             // { 
             //     type: "Video", 
-            //     url: "https://itccollegprot039-my.sharepoint.com/personal/ghr66_itccollegprot039_onmicrosoft_com/_layouts/15/stream.aspx?id=%2Fpersonal%2Fghr66%5Fitccollegprot039%5Fonmicrosoft%5Fcom%2FDocuments%2FRecordings%2FVulnerability%20Analyst%20%20Penetration%20Tester%20%2D%20GLB%20%2D%20CAI2%5FISS3%5FG1%20%2D%203H%2D20241109%5F193638%2DMeeting%20Recording%2Emp4&nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJTdHJlYW1XZWJBcHAiLCJyZWZlcnJhbFZpZXciOiJTaGFyZURpYWxvZy1MaW5rIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXcifX0&ga=1&referrer=StreamWebApp%2EWeb&referrerScenario=AddressBarCopied%2Eview%2E80ca8af6%2Db99c%2D4110%2Dad39%2D1b11e30322d0", 
+            //     url: "", 
+            //     label: "Lecture 16 (Video)", 
+            //     category: "Technical" 
+            // },
+            // { 
+            //     type: "PDF", 
+            //     url: "", 
+            //     label: "Lecture 16 (PDF)", 
+            //     category: "Technical" 
+            // },
+            // { 
+            //     type: "Video", 
+            //     url: "", 
+            //     label: "Lecture 17 (Video)", 
+            //     category: "Non-Technical" 
+            // },
+            // { 
+            //     type: "PDF", 
+            //     url: "", 
+            //     label: "Lecture 17 (PDF)", 
+            //     category: "Non-Technical" 
+            // }
+        ]
+    },
+    {
+        week: "Week 7",
+        lectures: [
+            { 
+                type: "Video", 
+                url: "https://itccollegprot039-my.sharepoint.com/:v:/g/personal/ghr66_itccollegprot039_onmicrosoft_com/EYf4LNTFno9CoXAxpRFVH6ABxHWkDNUO1xYLQIvCyUfkQg?e=3FOjqA&nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJTdHJlYW1XZWJBcHAiLCJyZWZlcnJhbFZpZXciOiJTaGFyZURpYWxvZy1MaW5rIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXcifX0%3D", 
+                label: "Cyber Threat Intelligence Lecture 18 (Video)", 
+                category: "Technical" 
+            },
+            { 
+                type: "PDF", 
+                url: "https://drive.google.com/file/d/1OYOHLujNfrjm8n6Q6V82KJrZ9NZftEro/view?usp=drive_link", 
+                label: "Cyber Threat Intelligence Lecture 18 (PDF)", 
+                category: "Technical" 
+            },
+            { 
+                type: "Video", 
+                url: "https://itccollegprot039-my.sharepoint.com/:v:/g/personal/ghr66_itccollegprot039_onmicrosoft_com/ERP00GV1JG1KoghYK-lxh2ABKcpgz5-AvWbMHwXqvwQwmg?e=aFxPbs&nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJTdHJlYW1XZWJBcHAiLCJyZWZlcnJhbFZpZXciOiJTaGFyZURpYWxvZy1MaW5rIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXcifX0%3D", 
+                label: "Red Teaming and Blue Teaming Lecture 19 (Video)", 
+                category: "Technical" 
+            },
+            { 
+                type: "PDF", 
+                url: "https://drive.google.com/file/d/1bRjdnFj29ptUuUKKFKaskroHT1oG77gO/view?usp=drive_link", 
+                label: "Red Teaming and Blue Teaming Lecture 19  (PDF)", 
+                category: "Technical" 
+            },
+            { 
+                type: "Video", 
+                url: "https://itccollegprot039-my.sharepoint.com/personal/ghr66_itccollegprot039_onmicrosoft_com/_layouts/15/stream.aspx?id=%2Fpersonal%2Fghr66_itccollegprot039_onmicrosoft_com%2FDocuments%2FRecordings%2FVulnerability%20Analyst%20%20Penetration%20Tester%20-%20GLB%20-%20CAI2_ISS3_G1%20-%203H-20241207_201811-Meeting%20Recording%2Emp4&nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJTdHJlYW1XZWJBcHAiLCJyZWZlcnJhbFZpZXciOiJTaGFyZURpYWxvZy1MaW5rIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXcifX0&ga=1&referrer=StreamWebApp%2EWeb&referrerScenario=AddressBarCopied%2Eview%2E123fce07-a27f-412e-bff0-de712ed993d3", 
+                label: "Portfolio Lecture 20 (Video)", 
+                category: "Non-Technical" 
+            },
+            { 
+                type: "PDF", 
+                url: "https://drive.google.com/file/d/19zVHVXfou-xbWcghCO7FW7x8R8nllxOq/view", 
+                label: "Portfolio Lecture 20  (PDF)", 
+                category: "Non-Technical" 
+            }
+        ]
+    },
+    {
+        week: "Week 8",
+        lectures: [
+            { 
+                type: "Video", 
+                url: "https://itccollegprot039-my.sharepoint.com/personal/ghr66_itccollegprot039_onmicrosoft_com/_layouts/15/stream.aspx?id=%2Fpersonal%2Fghr66%5Fitccollegprot039%5Fonmicrosoft%5Fcom%2FDocuments%2FRecordings%2FVulnerability%20Analyst%20%20Penetration%20Tester%20%2D%20GLB%20%2D%20CAI2%5FISS3%5FG1%20%2D%203H%2D20241219%5F194238%2DMeeting%20Recording%2Emp4&nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJTdHJlYW1XZWJBcHAiLCJyZWZlcnJhbFZpZXciOiJTaGFyZURpYWxvZy1MaW5rIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXcifX0&ga=1&referrer=StreamWebApp%2EWeb&referrerScenario=AddressBarCopied%2Eview%2E074a44f2%2Dbb46%2D4fd0%2Db2b6%2Dd9533a459c90", 
+                label: "Network Penetration Testing Lecture 21 (Video)", 
+                category: "Technical" 
+            },
+            { 
+                type: "PDF", 
+                url: "https://drive.google.com/file/d/1AOfJyNhQ3BWiBY2WyHBuejKHE-hNkolF/view?usp=drive_link", 
+                label: "Network Penetration Testing Lecture 21  (PDF)", 
+                category: "Technical" 
+            },
+            { 
+                type: "PDF", 
+                url: "https://drive.google.com/file/d/1V0NkGueJjwIO0hUdIynRCr_0Gn8IXaZz/view?usp=drive_link", 
+                label: "Wireless Network Penetration Testing Lecture 21  (PDF)", 
+                category: "Technical" 
+            },
+            { 
+                type: "Video", 
+                url: "https://itccollegprot039-my.sharepoint.com/personal/ghr66_itccollegprot039_onmicrosoft_com/_layouts/15/stream.aspx?id=%2Fpersonal%2Fghr66%5Fitccollegprot039%5Fonmicrosoft%5Fcom%2FDocuments%2FRecordings%2FVulnerability%20Analyst%20%20Penetration%20Tester%20%2D%20GLB%20%2D%20CAI2%5FISS3%5FG1%20%2D%203H%2D20241220%5F094953%2DMeeting%20Recording%2Emp4&nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJTdHJlYW1XZWJBcHAiLCJyZWZlcnJhbFZpZXciOiJTaGFyZURpYWxvZy1MaW5rIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXcifX0&ga=1&referrer=StreamWebApp%2EWeb&referrerScenario=AddressBarCopied%2Eview%2E33f1dfd4%2Df508%2D43cd%2Db780%2D3a7bf0f374db", 
+                label: "Social Engineering Techniques and Tools Lecture 22 (Video)", 
+                category: "Technical" 
+            },
+            { 
+                type: "PDF", 
+                url: "https://drive.google.com/drive/folders/1yOVojPL9px3LDCWC6sAjmZqhUKAT1nDn", 
+                label: "Social Engineering Techniques and Tools Lecture 22 (PDF)", 
+                category: "Technical" 
+                //20/12/2024
+            },
+            // { 
+            //     type: "Video", 
+            //     url: "", 
             //     label: "Lecture 8 (Video)", 
             //     category: "Non-Technical" 
             // },
             // { 
             //     type: "PDF", 
-            //     url: "https://drive.google.com/drive/folders/1PBloDz6WCCYjiBoocVP7kuHoalSw_7Nk", 
+            //     url: "", 
             //     label: "Lecture 8 (PDF)", 
             //     category: "Non-Technical" 
             // }
         ]
     },
+    // {
+    //     week: "Week 6",
+    //     lectures: [
+    //         { 
+    //             type: "Video", 
+    //             url: "", 
+    //             label: "Lecture 1 (Video)", 
+    //             category: "Technical" 
+    //         },
+    //         { 
+    //             type: "PDF", 
+    //             url: "", 
+    //             label: "Lecture 1 (PDF)", 
+    //             category: "Technical" 
+    //         },
+    //         { 
+    //             type: "Video", 
+    //             url: "", 
+    //             label: "Lecture 13 (Video)", 
+    //             category: "Technical" 
+    //         },
+    //         { 
+    //             type: "PDF", 
+    //             url: "", 
+    //             label: "Lecture 1 (PDF)", 
+    //             category: "Technical" 
+    //         },
+    //         { 
+    //             type: "Video", 
+    //             url: "", 
+    //             label: "Lecture 8 (Video)", 
+    //             category: "Non-Technical" 
+    //         },
+    //         { 
+    //             type: "PDF", 
+    //             url: "", 
+    //             label: "Lecture 8 (PDF)", 
+    //             category: "Non-Technical" 
+    //         }
+    //     ]
+    // },
 ];
